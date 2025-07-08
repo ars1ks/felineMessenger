f32f88430d5a0d160ed6a4026c41c3f9d41da82e
ars1ks <arszxc0101@yandex.ru>
2025-07-08 21:33:25 +0000

    vrotebal

---
+++ feline-messenger-frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AuthPage from './pages/AuthPage';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-background">
+      <AuthPage />
+    </div>
+  );
+}
+
+export default App;
